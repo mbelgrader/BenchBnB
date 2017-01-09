6d5abb868a034216b76a0c4d0fbf730299282a39
Maria Belgrader <mbelgrader5@gmail.com>
2017-01-09 13:27:59 -0800

    Add bench backend with seeds

---
+++ app/controllers/api/benches_controller.rb
@@ -0,0 +1,17 @@
+class Api::BenchesController < ApplicationController
+  def index
+    @benches = Bench.all
+    render :index
+  end
+
+  def create
+    @bench = Bench.create!(bench_params)
+    render :show
+  end
+
+  private
+
+  def bench_params
+    params.require(:bench).permit(:lat, :lng, :description)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bench.create!(description: 'Potrero bench', lng: 37.754621, lat: -122.401171)
+Bench.create!(description: 'Right near benders', lng: 37.7600064, lat: 122.4194107)
+Bench.create!(description: 'Down the street from the Alamo Theater', lng: 37.756222, lat: -122.4213112)
+Bench.create!(description: 'Dolores Park', lng: 37.7592704, lat: -122.4286812)
+Bench.create!(description: 'Botanical Garden at Golden Gate Park', lng: 37.7652855, lat: -122.4673965)
+Bench.create!(description: 'Palace of Fine Arts', lng: 37.7845264, lat: -122.4580911)
